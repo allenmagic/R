1bf05eac2505d0af73cd1ac39957a48dda5058bc
allenmagic <zheng.youxin@gmail.com>
2024-02-01 16:41:42 +0800

    Update crm_test.R

--- test/crm_test.R
+++ test/crm_test.R
@@ -2,12 +2,7 @@
 library(RMySQL) 
 
 # 建立数据库链接，CRM数据库地址：10.11.6.13
-dbconn <- dbConnect(
-  MySQL(), host = '10.11.6.13', 
-  dbname = 'irs', 
-  user = 'irs', 
-  password = 'irs_12qwaszx!@QWASZX', 
-  port = 3306)
+
 
 # 获取联系人信息
 ir_contactors <- dbSendQuery(
